fb13217653106a191ba9427fa5d96bbbe6dbbd08
Bowen Zheng <bowen27@mit.edu>
2024-05-23 22:32:00 -0400

    testing with 200 trials

--- experiment.js
+++ experiment.js
@@ -35,8 +35,8 @@ document.addEventListener('DOMContentLoaded', function() {
 
 
     const maxPracticeTrialsCount = 200;
-    const practiceTrialsCount = 5;
-    const formalTrialsCount = 5;
+    const practiceTrialsCount = 50;
+    const formalTrialsCount = 200;
     let flashOn = 0;
     let trialCount = 0;
     let isPractice = true;
